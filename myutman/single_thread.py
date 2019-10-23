a0fc8dd40a76fbf74aa6eb385bfc9593084f82ea
Mikhail Yutman <myutman@gmail.com>
2019-10-23 17:10:51 +0300

    refactoring: added class for reference-sliding window pair

--- myutman/single_thread.py
+++ myutman/single_thread.py
@@ -31,65 +31,75 @@ def kholmogorov_smirnov_dist(reference_window, sliding_window):
         #print(p, cur, mx)
     return mx
 
-class WindowStreamingAlgo(StreamingAlgo):
+class WindowPair:
 
-    def __init__(self, p, l, window_count, window_sizes, dist = kholmogorov_smirnov_dist):
-        super().__init__(p)
-        print(self.p)
-        if window_count != len(window_sizes):
-            raise IncorrectDataException()
-        self.l = l
-        self.window_count = window_count
-        self.maxes = [[0 for _ in range(l)] for _ in range(window_count)]
-        self.reference_windows = [[[] for _ in range(l + 1)] for _ in range(window_count)]
-        self.sliding_windows = [[[] for _ in range(l + 1)] for _ in range(window_count)]
-        self.window_sizes = window_sizes
+    def __init__(self, sizes, dist=kholmogorov_smirnov_dist):
+        self.sizes = sizes
         self.__dist = dist
+        self.reference = []
+        self.sliding = []
+        self.max = 0
+        self.stat = 0
         self.grace = 0
 
-    def process_element(self, element):
+    def add_point(self, point):
         self.grace += 1
-        for k in range(self.window_count):
-            if len(self.reference_windows[k][0]) < self.window_sizes[k][0]:
-                self.reference_windows[k][0].append(element)
-            if len(self.sliding_windows[k][0]) == self.window_sizes[k][1]:
-                self.sliding_windows[k][0] = self.sliding_windows[k][0][1:]
-            self.sliding_windows[k][0].append(element)
-            for j in range(1, self.l + 1):
-                new_val = np.random.uniform(0, 1)
-                if len(self.reference_windows[k][j]) < self.window_sizes[k][0]:
-                    self.reference_windows[k][j].append(new_val)
-                if len(self.sliding_windows[k][j]) == self.window_sizes[k][1]:
-                    self.sliding_windows[k][j] = self.sliding_windows[k][j][1:]
-                self.sliding_windows[k][j].append(new_val)
-                if self.grace >= self.window_sizes[k][0] + self.window_sizes[k][1]:
-                    self.maxes[k][j - 1] = max(self.maxes[k][j - 1],
-                        self.__dist(self.reference_windows[k][j], self.sliding_windows[k][j]))
+        if len(self.reference) < self.sizes[0]:
+            self.reference.append(point)
+        if len(self.sliding) == self.sizes[1]:
+            self.sliding = self.sliding[1:]
+        self.sliding.append(point)
+        if self.grace >= self.sizes[0] + self.sizes[1]:
+            self.stat = self.__dist(self.reference, self.sliding)
+            self.max = max(self.max, self.stat)
+
+    def get_max(self):
+        return self.max
 
     def get_stat(self):
-        return [0 if self.grace < self.window_sizes[i][0] + self.window_sizes[i][1]
-                else self.__dist(self.reference_windows[i][0], self.sliding_windows[i][0])
-                for i in range(self.window_count)]
+        return self.stat
 
-    def get_threshholds(self):
-        threshholds = []
+
+class WindowStreamingAlgo(StreamingAlgo):
+
+    def __init__(self, p, l, window_sizes, dist=kholmogorov_smirnov_dist):
+        super().__init__(p)
+        print(self.p)
+        self.l = l
+        self.window_count = len(window_sizes)
+        self.window_pairs = [[WindowPair(sizes, dist) for _ in range(l + 1)] for sizes in window_sizes]
+        self.__dist = dist
+
+    def process_element(self, element):
+        #self.grace += 1
         for k in range(self.window_count):
-            threshholds.append(np.quantile(self.maxes[k], 1 - self.p))
-        return threshholds
+            self.window_pairs[k][0].add_point(element)
+            for j in range(1, self.l + 1):
+                self.window_pairs[k][j].add_point(np.random.uniform(0, 1))
+
+    def get_stat(self):
+        return [self.window_pairs[i][0].get_stat() for i in range(self.window_count)]
+
+    def get_thresholds(self):
+        thresholds = []
+        for k in range(self.window_count):
+            maxes = [self.window_pairs[k][i].get_max() for i in range(1, self.l + 1)]
+            thresholds.append(np.quantile(maxes, 1 - self.p))
+        return thresholds
 
     def test(self):
-        return np.any(list(map(lambda x: x[0] < x[1], zip(self.get_threshholds(), self.get_stat()))))
+        return np.any(list(map(lambda x: x[0] < x[1], zip(self.get_thresholds(), self.get_stat()))))
 
 
 if __name__ == '__main__':
-    algo = WindowStreamingAlgo(0.05, 30, 3, [(71, 93), (80, 90), (65, 79)])
+    algo = WindowStreamingAlgo(0.05, 30, [(71, 93), (80, 90), (65, 79)])
     array = list(np.random.normal(0, 1, size = 1000))
     array += list(np.random.normal(0.2, 3, size = 1000))
 
     #print(kholmogorov_smirnov_dist(np.random.normal(0, 1, size = 100), np.random.normal(100, 1, size = 100)))
     for it, elem in enumerate(array):
         algo.process_element(elem)
-        print(algo.get_stat(), algo.get_threshholds())
+        print(algo.get_stat(), algo.get_thresholds())
         if algo.test():
             print(it)
             break

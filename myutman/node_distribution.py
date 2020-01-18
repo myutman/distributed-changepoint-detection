07184aa00ca3f0dc86a1a4c11efc6d630320ffae
Mikhail Yutman <myutman@gmail.com>
2020-01-18 14:23:33 +0300

    added another node distribution

--- myutman/node_distribution.py
+++ myutman/node_distribution.py
@@ -1,8 +1,8 @@
 import abc
-
+import numpy as np
 
 class NodeDistribution:
-    def __init__(self, n_nodes):
+    def __init__(self, n_nodes: int):
         self.n_nodes = n_nodes
 
     @abc.abstractmethod
@@ -11,7 +11,7 @@ class NodeDistribution:
 
 
 class RoundrobinNodeDistribution(NodeDistribution):
-    def __init__(self, n_nodes):
+    def __init__(self, n_nodes: int):
         super(RoundrobinNodeDistribution, self).__init__(n_nodes)
         self.cur = 0
 
@@ -22,8 +22,21 @@ class RoundrobinNodeDistribution(NodeDistribution):
 
 
 class DependentNodeDistribution(NodeDistribution):
-    def __init__(self, n_nodes):
+    def __init__(self, n_nodes: int):
         super(DependentNodeDistribution, self).__init__(n_nodes)
 
     def get_node_index(self, meta=None):
         return hash(meta) % self.n_nodes
+
+
+class VariableNodeDistribution(NodeDistribution):
+    def _init_(self, n_nodes: int, alpha: float, state: int = 0):
+        super(VariableNodeDistribution, self).__init__(n_nodes)
+        self.alpha = alpha
+        self.random_state = np.random.RandomState(state)
+
+    def get_node_index(self, meta=None):
+        if self.random_state.binomial(1, self.alpha) == 1:
+            return hash(meta) % self.n_nodes
+        else:
+            return self.random_state.choice(self.n_nodes)
